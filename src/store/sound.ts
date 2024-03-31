35d3658bc6849f5876d79f36a2772de62733e217
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-03-31 19:41:11 -0400

    set up voice model

--- src/store/sound.ts
+++ src/store/sound.ts
@@ -1,7 +1,12 @@
-import { Model, model, tProp, types } from 'mobx-keystone';
+import { Model, model, modelAction, tProp, types } from 'mobx-keystone';
 import { Voice } from './voice';
 
 @model('sound')
 export class Sound extends Model({
   voices: tProp(types.array(types.model(Voice)), () => [new Voice({})]),
-}) {}
+}) {
+  @modelAction
+  addVoice() {
+    this.voices.push(new Voice({}));
+  }
+}
